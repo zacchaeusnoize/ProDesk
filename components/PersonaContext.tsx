2e8640e5333eca9bdb44d3187346f730e46eeb0b
zacchaeusnoize <zacch.b@noize.com.au>
2026-05-29 14:47:18 +1000

    Delete components directory

--- components/PersonaContext.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-import { createContext, useContext } from 'react';
-
-export const PersonaCtx = createContext(null);
-export const usePersona = () => useContext(PersonaCtx) || { persona: null, setPersona: () => {}, chosen: false, reset: () => {} };
