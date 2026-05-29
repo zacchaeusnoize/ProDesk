ded392fb59bd930831bead847106f42b930211e0
zacchaeusnoize <zacch.b@noize.com.au>
2026-05-29 14:46:58 +1000

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Site from "../components/Site";
-
-export default function Page() {
-  return (
-    <div id="root" data-screen-label="01 Marketing site">
-      <Site />
-    </div>
-  );
-}
